0b6a7579f4930634eea142b0a75e47a65e145190
Jack Schuss <jackschuss@gmail.com>
2017-08-07 19:50:32 -0400

    Refactor FileChecker to return a struct

--- fileChecker/fileChecker.go
+++ fileChecker/fileChecker.go
@@ -1,24 +1,34 @@
-package fileChecker
+package main
 
 import (
-  "os";
   "time"
+  "github.com/spf13/afero"
 )
 
+type ModificationInfo struct {
+  modified bool
+  timeModified string
+}
 
 func main() {
 }
 
-func IsModified(f string) (bool, string) {
+func IsModified(fileName string, checker afero.Fs) ModificationInfo {
 
   diff := 24 * time.Hour
   twentyFourHoursAgo := time.Now().Add(-diff)
 
-  info, err := os.Stat(f)
+  info, err := checker.Stat(fileName)
 
   if err != nil {
-    return false, "not modified"
+    return ModificationInfo{
+      false,
+      "not modified",
+    }
   }
 
-  return info.ModTime().After(twentyFourHoursAgo), info.ModTime().Format("2006-01-02T15:04:05.999999-07:00")
+  return ModificationInfo{
+    info.ModTime().After(twentyFourHoursAgo),
+    info.ModTime().Format("2006-01-02T15:04:05.999999-07:00"),
+  }
 }
